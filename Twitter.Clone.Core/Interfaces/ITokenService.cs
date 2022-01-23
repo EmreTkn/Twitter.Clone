9cc0e54898f65042e5ce40ffbfa31323ba458780
Emre Tekin <61517900+EmreTkn@users.noreply.github.com>
2022-01-23 22:29:19 +0300

    Delete Twitter.Clone.Core directory

--- Twitter.Clone.Core/Interfaces/ITokenService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Twitter.Clone.Models.Concrete;
-
-namespace Twitter.Clone.Core.Interfaces;
-public interface ITokenService
-{
-    (string, string) CreateToken(IdentityUser user);
-    string CreateGuestToken(string name);
-}
--- Twitter.Clone.Core/Interfaces/ResponseCacheService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Twitter.Clone.Core.Interfaces;
-public interface IResponseCacheService
-{
-    Task CacheResponseAsync(string cacheKey, object response, TimeSpan timeToLive);
-    Task<string> GetCachedResponse(string cacheKey);
-}
